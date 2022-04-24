7838f59082f0aea5bb4646786465947bd43411e1
Kuroky-Chen <5837907@qq.com>
2022-04-24 21:35:46 +0800

    全部完成

--- src/views/IndustryMap/components/right.vue
+++ src/views/IndustryMap/components/right.vue
@@ -4,7 +4,7 @@
       <div class="t-left">
         <div class="t-l-top">
           <SubTitle title="产业链占比" />
-          <div class="box">
+          <div id="echarts2" class="box">
             12
           </div>
         </div>
@@ -111,10 +111,135 @@ export default {
     }
   },
   mounted() {
+    this.echarts2Init()
     this.echarts3Init()
     this.echarts4Init()
   },
   methods: {
+    echarts2Init() {
+      const chart = echarts.init(document.getElementById('echarts2'))
+      // 绘制图表
+      const scaleData = [
+        {
+          name: '数据项1',
+          value: 30
+        },
+        {
+          name: '数据项2',
+          value: 20
+        },
+        {
+          name: '数据项3',
+          value: 10
+        }
+      ]
+      const rich = {
+        white: {
+          color: '#ddd',
+          align: 'center',
+          padding: [3, 0]
+        }
+      }
+      const placeHolderStyle = {
+        normal: {
+          label: {
+            show: false
+          },
+          labelLine: {
+            show: false
+          },
+          color: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgba(0, 0, 0, 0)',
+          borderWidth: 0
+        }
+      }
+      const data = []
+      const color = [
+        '#00ffff',
+        '#00cfff',
+        '#006ced',
+        '#ffe000',
+        '#ffa800',
+        '#ff5b00',
+        '#ff3000'
+      ]
+      for (let i = 0; i < scaleData.length; i++) {
+        data.push(
+          {
+            value: scaleData[i].value,
+            name: scaleData[i].name,
+            itemStyle: {
+              normal: {
+                borderWidth: 4,
+                shadowBlur: 200,
+                borderColor: color[i],
+                shadowColor: color[i]
+              }
+            }
+          },
+          {
+            value: 2,
+            name: '',
+            itemStyle: placeHolderStyle
+          }
+        )
+      }
+      const seriesObj = [
+        {
+          name: '',
+          type: 'pie',
+          clockWise: false,
+          radius: [50, 100],
+          hoverAnimation: false,
+          itemStyle: {
+            normal: {
+              label: {
+                show: true,
+                position: 'outside',
+                color: '#ddd',
+                formatter: function (params) {
+                  let percent = 0
+                  let total = 0
+                  for (let i = 0; i < scaleData.length; i++) {
+                    total += scaleData[i].value
+                  }
+                  percent = ((params.value / total) * 100).toFixed(0)
+                  if (params.name !== '') {
+                    return params.name + '\n{white|' + '占比' + percent + '%}'
+                  } else {
+                    return ''
+                  }
+                },
+                rich: rich
+              },
+              labelLine: {
+                length: 10,
+                length2: 30,
+                show: true,
+                color: '#00ffff'
+              }
+            }
+          },
+          data: data
+        }
+      ]
+      const option = {
+        tooltip: {
+          show: false
+        },
+        legend: {
+          show: false
+        },
+        toolbox: {
+          show: false
+        },
+        series: seriesObj
+      }
+
+      chart.setOption({
+        ...option
+      })
+    },
     echarts3Init() {
       const chart = echarts.init(document.getElementById('echarts3'))
       // 绘制图表
@@ -140,19 +265,29 @@ export default {
             },
             splitLine: {
               // 分隔线样式
-              show: true
+              show: true,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             axisLabel: {
               // 刻度标签
-              show: false
+              show: false,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             axisTick: {
               // 刻度样式
-              show: true
+              show: true,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             detail: {
               // 其余属性默认使用全局文本样式，详见TEXTSTYLE
               fontWeight: 'bolder',
+              color: '#fff',
               fontSize: 40,
               offsetCenter: [0, '20%']
             },
@@ -194,17 +329,27 @@ export default {
             },
             splitLine: {
               // 分隔线样式
-              show: true
+              show: true,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             axisLabel: {
               // 刻度标签
-              show: true
+              show: false,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             axisTick: {
               // 刻度样式
-              show: true
+              show: true,
+              lineStyle: {
+                color: '#ffffff'
+              }
             },
             detail: {
+              color: '#fff',
               // 其余属性默认使用全局文本样式，详见TEXTSTYLE
               fontWeight: 'bolder',
               fontSize: 40,
@@ -346,6 +491,10 @@ export default {
 
       .t-l-bottom {
         @include chart;
+
+        .box {
+          padding-top: 30px;
+        }
       }
     }
 
