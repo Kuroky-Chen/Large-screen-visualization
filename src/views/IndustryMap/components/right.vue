5d18ad45e80b42d780bdd9946dbe8480028156cb
Kuroky-Chen <5837907@qq.com>
2022-05-10 14:45:04 +0800

    更改仪表盘

--- src/views/IndustryMap/components/right.vue
+++ src/views/IndustryMap/components/right.vue
@@ -246,118 +246,237 @@ export default {
       const option = {
         series: [
           {
-            name: '内圈小',
+            name: '内层数据刻度',
             type: 'gauge',
-            pointer: {
-              show: false
-            },
-            radius: '70%',
-            startAngle: 200,
-            endAngle: -20,
-            splitNumber: 4,
+            radius: '92',
+            min: 0,
+            max: 100,
+            center: ['50%', '55%'],
+            startAngle: 186,
+            endAngle: -6,
+            splitNumber: 5,
+
             axisLine: {
-              // 坐标轴线
+              roundCap: true,
               lineStyle: {
-                // 属性lineStyle控制线条样式
-                color: [[1, '#bfcbd9']],
-                width: 20
+                width: 12,
+                // color: [[0.2, '#77D97F'],[0.8, '#4285F4'],[1, '#FF7E7F']],
+                color: [[1, '#3383a2']]
               }
             },
-            splitLine: {
-              // 分隔线样式
+            progress: {
               show: true,
-              lineStyle: {
-                color: '#ffffff'
-              }
-            },
-            axisLabel: {
-              // 刻度标签
-              show: false,
-              lineStyle: {
-                color: '#ffffff'
+              roundCap: true,
+              width: 11,
+              itemStyle: {
+                color: {
+                  type: 'linear',
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: '#ffffff'
+                    },
+                    {
+                      offset: 0.3,
+                      color: '#54cae2'
+                    },
+                    {
+                      offset: 0.7,
+                      color: '#3690a3'
+                    },
+                    {
+                      offset: 1,
+                      color: '#2d7a8b'
+                    }
+                  ]
+                  // global: false // 缺省为 false
+                },
+                borderColor: '#1b5b76',
+                borderWidth: '2'
               }
             },
             axisTick: {
-              // 刻度样式
               show: true,
+              splitNumber: 5,
+              length: 8,
+              distance: -10,
               lineStyle: {
+                width: 7,
+                color: '#1c5a73'
+              }
+            },
+            splitLine: {
+              show: true,
+              length: 1,
+              lineStyle: {
+                width: 2,
                 color: '#ffffff'
               }
             },
+
+            axisLabel: {
+              distance: 5,
+              color: '#FFF',
+              fontSize: 11
+            },
+            pointer: {
+              length: '60%',
+              width: 2,
+              offsetCenter: [0, '-40%'],
+              itemStyle: {
+                color: {
+                  type: 'linear',
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: '#ffffff'
+                    },
+                    {
+                      offset: 0.7,
+                      color: '#ffffff'
+                    },
+                    {
+                      offset: 1,
+                      color: '#2d7a8b'
+                    }
+                  ]
+                  // global: false // 缺省为 false
+                },
+                // shadowColor: 'rgba(0, 0, 0, 0.5)',
+                shadowBlur: 7,
+                shadowColor: '#ffffff'
+              }
+            },
             detail: {
-              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-              fontWeight: 'bolder',
-              color: '#fff',
-              fontSize: 40,
-              offsetCenter: [0, '20%']
+              offsetCenter: ['4%', '-5%'],
+              formatter: function (value) {
+                return '{value|' + value.toFixed(0) + '}{unit|%}'
+              },
+              rich: {
+                value: {
+                  fontSize: 28,
+                  fontWeight: 'bolder',
+                  color: '#ffffff',
+                  textShadowBlur: '12',
+                  textShadowColor: '#3ea0b5'
+                },
+                unit: {
+                  fontSize: 28,
+                  fontWeight: 'bolder',
+                  color: '#ffffff',
+                  textShadowBlur: '12',
+                  textShadowColor: '#3ea0b5'
+                }
+              }
             },
             data: [
               {
-                value: 60
+                value: 88
               }
             ]
           },
           {
-            name: '内圈小',
             type: 'gauge',
-            title: {
-              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-              fontWeight: 'bolder',
-              fontSize: 30,
-              fontStyle: 'italic',
-              offsetCenter: [0, '33%']
-            },
+            name: '外层辅助',
+            radius: 100,
+            startAngle: 186,
+            center: ['50%', '55%'],
+            endAngle: -6,
+            min: 0,
+            max: 100,
+            splitNumber: 5,
             pointer: {
-              show: true
+              show: false
             },
-            radius: '70%',
-            startAngle: 200,
-            endAngle: 50,
-            splitNumber: 4,
             axisLine: {
-              // 坐标轴线
-              lineStyle: {
-                // 属性lineStyle控制线条样式
-                color: [[1, '#0093ee']],
-                width: 20,
-                shadowColor: '#0093ee', // 默认透明
-                shadowOffsetX: 0,
-                shadowOffsetY: 0,
-                shadowBlur: 40,
-                opacity: 1
-              }
-            },
-            splitLine: {
-              // 分隔线样式
+              roundCap: true,
               show: true,
               lineStyle: {
-                color: '#ffffff'
+                color: [[1, '#3ea0b5']],
+                width: 3,
+                opacity: 0.5,
+                shadowColor: 'rgba(0,138,255,0.45)',
+                shadowBlur: 5,
+                shadowOffsetX: 1,
+                shadowOffsetY: 1
+              }
+            },
+
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              show: false
+            },
+            axisLabel: {
+              show: false
+            }
+          },
+          {
+            name: '最内层线',
+            type: 'gauge',
+            radius: '64',
+            center: ['50%', '55%'],
+            startAngle: 186,
+            endAngle: -6,
+
+            splitLine: {
+              show: false,
+              lineStyle: {
+                opacity: 0
               }
             },
             axisLabel: {
-              // 刻度标签
-              show: false,
+              show: false
+            },
+            // 上面一圈
+            itemStyle: {
+              color: '#3ea0b5',
+              shadowColor: 'rgba(0,138,255,0.45)',
+              shadowBlur: 2,
+              shadowOffsetX: 1,
+              shadowOffsetY: 1
+            },
+            // 刻度指到位置
+            progress: {
+              roundCap: true,
+              show: true,
+              roundCap: true,
+              width: 3
+            },
+            axisLine: {
+              roundCap: true,
+              show: true,
               lineStyle: {
-                color: '#ffffff'
+                color: [[1, '#3ea0b5']],
+                width: 3,
+                opacity: 0.5,
+                shadowColor: 'rgba(0,138,255,0.45)',
+                shadowBlur: 5,
+                shadowOffsetX: 1,
+                shadowOffsetY: 1
               }
             },
             axisTick: {
-              // 刻度样式
-              show: true,
-              lineStyle: {
-                color: '#ffffff'
-              }
+              show: false
+            },
+            pointer: {
+              show: false
             },
             detail: {
-              color: '#fff',
-              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-              fontWeight: 'bolder',
-              fontSize: 40,
-              offsetCenter: [0, '20%']
+              show: false
             },
             data: [
               {
-                value: '60'
+                value: 88
               }
             ]
           }
